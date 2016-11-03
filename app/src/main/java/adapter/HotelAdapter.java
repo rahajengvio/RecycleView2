84f43ad72da555c183ecc32302b7cb6be13cdd3b
Rahajeng <rahajeng_violita_23rpl@student.smktelkom-mlg.sch.id>
2016-11-04 06:45:29 +0700

    RecycleView List

---
+++ app/src/main/java/adapter/HotelAdapter.java
@@ -0,0 +1,65 @@
+package adapter;
+
+import android.media.Image;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+import id.sch.smktelkom_mlg.learn.recyclerview1.R;
+import model.hotel;
+
+/**
+ * Created by user_ on 11/3/2016.
+ */
+
+public class HotelAdapter extends RecyclerView.Adapter<HotelAdapter.ViewHolder>{
+    ArrayList<hotel> hotelList;
+
+    public HotelAdapter(ArrayList<hotel> hotelList){
+        this.hotelList = hotelList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_list,parent,false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        hotel hotel = hotelList.get(position);
+        holder.tvJudul.setText(hotel.judul);
+        holder.tvDeskripsi.setText(hotel.deskripsi);
+        holder.ivFoto.setImageDrawable(hotel.foto);
+    }
+
+    @Override
+    public int getItemCount() {
+        if(hotelList!=null)
+            return hotelList.size();
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView ivFoto;
+        TextView tvJudul;
+        TextView tvDeskripsi;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            ivFoto = (ImageView) itemView.findViewById(R.id.imageView);
+            tvJudul = (TextView) itemView.findViewById(R.id.textViewJudul);
+            tvDeskripsi = (TextView) itemView.findViewById(R.id.textViewDeskripsi);
+
+        }
+    }
+}
